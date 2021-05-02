a2af00fdaba6644b604757941be54ded8678a55c
Philip Mattsson <philip.mattsson@iths.se>
2021-05-02 18:58:15 +0200

    Version 0.8

--- src/main/java/atmMachine/Account.java
+++ src/main/java/atmMachine/Account.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class Account {
 
-    private static Scanner in;
     private static float balance = 4000;
-    private static int anotherTransaction;
 
     public static void withdraw() {
 
+        Scanner scan = new Scanner(System.in);
         float amount;
 
         System.out.println("Please enter amount to withdraw: ");
-        amount = in.nextFloat();
+        amount = scan.nextFloat();
         if (amount > balance || amount == 0) {
             System.out.println("You have insufficient funds\n\n");
-            anotherTransaction(); // ask if they want another transaction
+            anotherTransaction();
         } else {
-            // they have some cash
-            // update balance
             balance = balance - amount;
             System.out.println("You have withdrawn " + amount + " and your new balance is " + balance + "\n");
         }
     }
 
         public static void balance(){
+
             System.out.println("Your balance is " + balance + "\n");
             anotherTransaction();
         }
 
     private static void anotherTransaction() {
+
+        Scanner scan = new Scanner(System.in);
         System.out.println("Do you want another transaction?\n\nPress 1 for another transaction\n2 To exit");
-        anotherTransaction = in .nextInt();
+        int anotherTransaction = scan.nextInt();
         if (anotherTransaction == 1) {
-//            transaction(); // call transaction method
+            withdraw();
         } else if (anotherTransaction == 2) {
             System.out.println("Thanks for choosing us. Good Bye!");
         } else {
